35e6c0cc9cad6ba3d301cb0e36b57cd7010f6f54
Timo Erkkila <timo.erkkila@gmail.com>
2014-01-13 16:51:10 -0800

    Added binary for logistic regression.

--- README.md
+++ README.md
@@ -3,7 +3,16 @@ cgml
 
 Machine Learning with Computational Graphs
 
+
 installation
 ============
 
 python setup.py install 
+
+
+running
+=======
+
+cgml-logreg
+cgml-mlp
+cgml-ae
--- setup.py
+++ setup.py
@@ -1,7 +1,14 @@
+# coding=utf-8
+
 
 from distutils.core import setup
 
-setup( name        = 'cgml',
-       version     = '1.0',
-       packages    = ['cgml'],
-       package_dir = {'cgml': 'src/cgml'} )
+setup( name         = 'cgml',
+       version      = '1.0',
+       description  = 'Machine Learning with Computational Graphs',
+       author       = 'Timo Erkkilä',
+       author_email = 'timo.erkkila@gmail.com',
+       url          = 'http://github.com/terkkila/cgml/',
+       packages     = ['cgml'],
+       package_dir  = {'cgml': 'src/cgml'},
+       data_files   = [('bin', ['bin/cgml-logreg'])])
