357fe8848406cbc8e08506b35e7796146ac4b6fa
terkkila <timo.erkkila@gmail.com>
2014-06-14 21:20:04 -0400

    Added unit tests for autoencoder.

---
+++ test/test_autoencoder.py
@@ -0,0 +1,55 @@
+
+import theano
+import theano.tensor as T
+import numpy as np
+from cgml.computational_graph import ComputationalGraph
+
+def test_autoencoder():
+
+    schema = {'description':'test CG',
+              'supervised-cost': {'type': 'negative-log-likelihood', 
+                                  'name': 'class-out'},
+              'unsupervised-cost': {'type': 'squared-error',
+                                    'name': 'decode-out'},
+              'graph':
+                  [{'activation':'linear',
+                    'n_in':5,
+                    'n_out':2,
+                    'dropout':0.0,
+                    'name':'encode-out',
+                    'branch':
+                        [{'activation':'softmax',
+                          'n_in':2,
+                          'n_out':2,
+                          'name':'class-out',
+                          'dropout':0.0}]},
+                   {'activation':'linear',
+                    'n_in':2,
+                    'n_out':5,
+                    'dropout':0.0,
+                    'name':'decode-out'}]}
+
+    model = ComputationalGraph(schema = schema,
+                               learnRate = 0.01,
+                               momentum = 0.0,
+                               seed = 0)
+
+    nLayers = len(model.layers)
+
+    assert nLayers == 3
+    for i in xrange(nLayers):
+        assert not np.any(model.layers[i].weights[1])
+
+    x = np.asarray([[1,2,3,4,5],[2,3,4,5,6]]).astype(theano.config.floatX)
+    y = np.asarray([0,1],dtype=np.int).reshape((2,)).astype(np.int32)
+    
+    x_dec = model.decode(x)
+
+    for i in xrange(1,x_dec.shape[0]):
+        assert np.all(x_dec[i-1] != x_dec[i])
+
+    for i in xrange(10):
+        model.hybrid_update(x,y)
+
+    assert not np.any(model.layers[0].weights()[0] == model.layers[1].weights()[0].T)
+    assert not np.any(model.layers[0].weights()[1] == model.layers[1].weights()[1])
