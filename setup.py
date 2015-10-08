bfdfbcb73e3409e51327ea3126d301c0974164b1
terkkila <timo.erkkila@gmail.com>
2015-10-08 10:23:38 +0300

    Bumped version number to 1.0.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ use_setuptools()
 from setuptools import setup,find_packages
 
 setup( name         = 'cgml',
-       version      = '1.0',
+       version      = '1.0.1',
        description  = 'Machine Learning with Computational Graphs',
        author       = 'Timo Erkkilä',
        author_email = 'timo.erkkila@gmail.com',
